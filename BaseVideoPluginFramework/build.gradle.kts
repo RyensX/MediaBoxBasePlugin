99f80f1ba62e73f509176658cde309985edcba41
RyensX <ryensu@qq.com>
2024-01-14 20:03:25 +0800

    fix: 修复依赖问题

--- BaseVideoPluginFramework/build.gradle.kts
+++ BaseVideoPluginFramework/build.gradle.kts
@@ -34,7 +34,7 @@ android {
 }
 
 dependencies {
-    implementation(project(mapOf("path" to ":MediaBoxPluginApi")))
+    api(project(mapOf("path" to ":MediaBoxPluginApi")))
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("com.google.android.material:material:1.5.0")
